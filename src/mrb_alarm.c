ce6df11f70f49324e860282bcad43e13e17a6122
0291 <PMAC280S@PMAC280S.local>
2015-11-04 12:09:46 +0900

    update

--- src/mrb_alarm.c
+++ src/mrb_alarm.c
@@ -6,62 +6,43 @@
 ** See Copyright Notice in LICENSE
 */
 
+#include <unistd.h>
+#include <signal.h>
+
 #include "mruby.h"
 #include "mruby/data.h"
 #include "mrb_alarm.h"
 
 #define DONE mrb_gc_arena_restore(mrb, 0);
 
-typedef struct {
-  char *str;
-  int len;
-} mrb_alarm_data;
-
-static const struct mrb_data_type mrb_alarm_data_type = {
-  "mrb_alarm_data", mrb_free,
-};
-
 static mrb_value mrb_alarm_init(mrb_state *mrb, mrb_value self)
 {
-  mrb_alarm_data *data;
-  char *str;
-  int len;
-
-  data = (mrb_alarm_data *)DATA_PTR(self);
-  if (data) {
-    mrb_free(mrb, data);
-  }
-  DATA_TYPE(self) = &mrb_alarm_data_type;
-  DATA_PTR(self) = NULL;
-
-  mrb_get_args(mrb, "s", &str, &len);
-  data = (mrb_alarm_data *)mrb_malloc(mrb, sizeof(mrb_alarm_data));
-  data->str = str;
-  data->len = len;
-  DATA_PTR(self) = data;
-
   return self;
 }
 
-static mrb_value mrb_alarm_hello(mrb_state *mrb, mrb_value self)
+static mrb_value mrb_instance_alarm(mrb_state *mrb, mrb_value self)
 {
-  mrb_alarm_data *data = DATA_PTR(self);
-
-  return mrb_str_new(mrb, data->str, data->len);
+   mrb_int time;
+   mrb_get_args(mrb, "i", &time);
+   alarm(time);
+   return mrb_nil_value();
 }
 
-static mrb_value mrb_alarm_hi(mrb_state *mrb, mrb_value self)
+static mrb_value mrb_class_alarm(mrb_state *mrb, mrb_value self)
 {
-  return mrb_str_new_cstr(mrb, "hi!!");
+   mrb_int time;
+   mrb_get_args(mrb, "i", &time);
+   alarm(time);
+   return mrb_nil_value();
 }
 
 void mrb_mruby_alarm_gem_init(mrb_state *mrb)
 {
     struct RClass *alarm;
     alarm = mrb_define_class(mrb, "Alarm", mrb->object_class);
-    mrb_define_method(mrb, alarm, "initialize", mrb_alarm_init, MRB_ARGS_REQ(1));
-    mrb_define_method(mrb, alarm, "hello", mrb_alarm_hello, MRB_ARGS_NONE());
-    mrb_define_class_method(mrb, alarm, "foo", mrb_alarm_hi, MRB_ARGS_NONE());
+    mrb_define_method(mrb, alarm, "initialize", mrb_alarm_init, MRB_ARGS_NONE());
+    mrb_define_method(mrb, alarm, "alarm", mrb_instance_alarm, MRB_ARGS_REQ(1));
+    mrb_define_class_method(mrb, alarm, "alarm", mrb_class_alarm, MRB_ARGS_REQ(1));
     DONE;
 }
 
